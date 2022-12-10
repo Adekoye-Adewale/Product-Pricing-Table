fb589ac2d5f6922b85e398762e1553c2944066a2
Adekoye Adewale <adekoyeadewale@gmail.com>
2022-12-11 00:51:54 +0100

    working

---
+++ script.js
@@ -0,0 +1,31 @@
+// var ricep=document.getElementById("riceprice");
+//     var riceq=document.getElementById("ricequantity");
+//     var ricet=document.getElementById("ricetotal");
+    
+//         ricep.value=100;
+//         riceq.value=("1");
+
+
+//     // let riceprice = "ricep.value";
+//     // let ricequantity = "riceq.value";
+
+//     // ricet.value=riceq.value * ricep.value;
+
+//     // document.getElementById("ricequantity").value = "vx";
+    
+// function riceresult() {
+//         document.getElementById("ricetotal").value = [riceq.value * ricep.value];
+//     };
+
+
+//     var maizep=document.getElementById("maizeprice");
+//     var maizeq=document.getElementById("maizequantity");
+//     var maizet=document.getElementById("maizetotal");
+    
+//         maizep.value=50;
+//         maizeq.value=("1");
+
+    
+// function riceresult() {
+//         document.getElementById("maizetotal").value = [maizeq.value * maizep.value];
+//     }
