7f5efc0141f84578b0274c4033ce8d93ddaca9a4
Adekoye Adewale <adekoyeadewale@gmail.com>
2022-12-11 01:22:20 +0100

    small '+' bug

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// var ricep=document.getElementById("riceprice");
-//     var riceq=document.getElementById("ricequantity");
-//     var ricet=document.getElementById("ricetotal");
+var ricep=document.getElementById("riceprice");
+    var riceq=document.getElementById("ricequantity");
+    var ricet=document.getElementById("ricetotal");
     
-//         ricep.value=100;
-//         riceq.value=("1");
+        ricep.value=100;
+        riceq.value=("1");
 
 
-//     // let riceprice = "ricep.value";
-//     // let ricequantity = "riceq.value";
+    // let riceprice = "ricep.value";
+    // let ricequantity = "riceq.value";
 
-//     // ricet.value=riceq.value * ricep.value;
+    // ricet.value=riceq.value * ricep.value;
 
-//     // document.getElementById("ricequantity").value = "vx";
+    // document.getElementById("ricequantity").value = "vx";
     
-// function riceresult() {
-//         document.getElementById("ricetotal").value = [riceq.value * ricep.value];
-//     };
+function riceresult() {
+        document.getElementById("ricetotal").value = [riceq.value * ricep.value];
+    };
 
 
-//     var maizep=document.getElementById("maizeprice");
-//     var maizeq=document.getElementById("maizequantity");
-//     var maizet=document.getElementById("maizetotal");
+    var maizep=document.getElementById("maizeprice");
+    var maizeq=document.getElementById("maizequantity");
+    var maizet=document.getElementById("maizetotal");
     
-//         maizep.value=50;
-//         maizeq.value=("1");
+        maizep.value=50;
+        maizeq.value=("1");
 
     
-// function riceresult() {
-//         document.getElementById("maizetotal").value = [maizeq.value * maizep.value];
-//     }
+function riceresult() {
+        document.getElementById("maizetotal").value = [maizeq.value * maizep.value];
+    }
